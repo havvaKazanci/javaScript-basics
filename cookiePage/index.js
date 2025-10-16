d2111bfbb0358f992b1752d84140af27102fd314
havvaKazanci <h.caglakazanci@gmail.com>
2025-10-16 14:02:16 +0300

    Cookie Consent project has added.

---
+++ cookiePage/index.js
@@ -0,0 +1,8 @@
+const consentContainer = document.querySelector("#consent-container")
+const acceptBtn = document.querySelector("#accept-btn")
+const consentText = document.getElementById("consent-text")
+
+setTimeout(function(){
+    consentContainer.style.display = "inline"
+},1500)
+
